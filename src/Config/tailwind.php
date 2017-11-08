f412f29a4424f17623ae13d174b84cf5223b3817
Aitor Riba <contact@aitorriba.com>
2017-11-08 22:11:38 +0100

    Navbar component

--- src/Config/tailwind.php
+++ src/Config/tailwind.php
@@ -1,3 +1,11 @@
 <?php
 
-return [];
+return [
+    'navbar' => [
+        'nav-classes' => 'bg-teal',
+        'title-classes' => 'text-white',
+        'menu-button-classes' => 'border rounded text-grey-lightest border-grey-lightest hover:text-white hover:border-white',
+        'items-classes' => "text-white",
+        'right-items-classes' => "border rounded px-4 py-2 hover:text-teal"
+    ]
+];
---
+++ src/Views/navbar.blade.php
@@ -0,0 +1,51 @@
+<nav class="flex items-center justify-between flex-wrap p-6 {{ config('tailwind.navbar.nav-classes') }}">
+    <div class="flex items-center flex-no-shrink mr-6">
+        @isset($image)
+            <img src="{{ $image }}" class="h-8 w-8 mr-2">
+        @endisset
+        @isset($title)
+            <span class="font-semibold text-xl tracking-tight {{ config('tailwind.navbar.title-classes') }}">{{ $title }}</span>
+        @endisset
+    </div>
+    <div class="block lg:hidden">
+        <button id="menu-button" class="flex items-center px-3 py-2 {{ config('tailwind.navbar.menu-button-classes') }}">
+            <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+    </div>
+    @isset($items)
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto" id="menu-items">
+        <div class="text-sm lg:flex-grow">
+            @foreach ($items as $name => $link)
+                <a href="{{ $link }}" class="block mt-4 lg:inline-block lg:mt-0 text-grey-lightest hover:text-white mr-4 no-underline {{ config('tailwind.navbar.items-classes') }}">
+                    {{ $name }}
+                </a>
+            @endforeach
+        </div>
+        @foreach ($rightItems as $name => $link)
+        <div class="ml-1">
+            <a href="{{ $link }}" class="inline-block text-sm leading-none text-white border-white hover:border-transparent hover:bg-white mt-4 lg:mt-0 no-underline {{ config('tailwind.navbar.right-items-classes') }}">
+                {{ $name }}
+            </a>
+        </div>
+    @endforeach
+    </div>
+    @endisset
+</nav>
+
+
+{{-- importing JQuery --}}
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+{{-- Code for hide and unhide menu on small devices --}}
+<script>
+    $(function () {
+        $('#menu-items').addClass('hidden');
+        $('#menu-button').click(function () {
+            if ($('#menu-items').hasClass('hidden')) {
+                $('#menu-items').removeClass('hidden');
+            } else {
+                $('#menu-items').addClass('hidden');
+            }
+        });
+    });
+    </script>
