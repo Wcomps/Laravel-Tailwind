4745fef9c96b849eec0cc179d2208da2eb6ec00f
Aitor Riba <contact@aitorriba.com>
2017-11-08 04:36:30 +0100

    Responsive cards and new card-sm

---
+++ src/Views/card-sm.blade.php
@@ -0,0 +1,26 @@
+<div class="xl:w-1/3 md:w-1/2 sm:w-full mt-4 px-4">
+    <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden xl:mt-6">
+        <div class="sm:flex sm:items-center px-6 py-4">
+            @isset($image)
+                <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="{{ $image }}" alt="">
+            @endisset
+            <div class="text-center sm:text-left sm:flex-grow">
+                <div class="mb-4">
+                    @isset($title)
+                        <p class="text-xl leading-tight">{{ $title }}</p>
+                    @endisset
+                    <p class="text-sm leading-tight text-grey-dark">
+                        {{ $slot }}
+                    </p>
+                </div>
+                @isset($buttons)
+                    <div>
+                    @foreach ($buttons as $title => $link)
+                        <a class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white no-underline" href="{{ $link }}">{{ $title }}</a>
+                    @endforeach
+                    </div>
+                @endisset
+            </div>
+        </div>
+    </div>
+</div>
